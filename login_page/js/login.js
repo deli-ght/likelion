2ef81a6c83fd66b1c3e6794a2ce7ffdefd1da702
delight <77158595+deli-ght@users.noreply.github.com>
2021-11-25 13:56:36 +0900

    1125

--- login_page/js/login.js
+++ login_page/js/login.js
@@ -13,3 +13,40 @@ main_login.addEventListener("click", () => {
 closeBtn.addEventListener("click", ()=> {
     modal.style.display = "none";
 })
+
+const input_id = document.querySelector("#id");
+const input_pw = document.querySelector("#pw");
+const login_btn = document.querySelector(".login_btn-login");
+
+login_btn.addEventListener("click", (e) =>{
+    if(input_id.value != "minju" || input_pw.value != '0415'){
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    if (!input_id.value){
+        input_id.classList.add("is_error");
+
+        const id_error = document.querySelector("#error_id");
+        id_error.classList.add("is_show");
+        setTimeout(()=> {
+            input_id.classList.remove("is_error");
+            id_error.classList.remove("is_show");},3000)
+    }
+    else if (!input_pw.value){
+        input_pw.classList.add("is_error");
+
+        const pw_error = document.querySelector("#error_pw");
+        pw_error.classList.add("is_show");
+        setTimeout(()=> {
+            input_pw.classList.remove("is_error");
+            pw_error.classList.remove("is_show");},3000)
+    }
+
+    else if(input_id.value != "minju" || input_pw.value != '0415'){
+        const wr_error = document.querySelector("#error_wr");
+        wr_error.classList.add("is_show");
+        setTimeout(()=> {
+            wr_error.classList.remove("is_show");},3000)
+    }
+})
